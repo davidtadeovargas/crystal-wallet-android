60df3ae6f9e44308816b451803d25512d888b094
henry <henry.varona@gmail.com>
2017-09-26 22:48:34 -0400

    Added pgrahenej library Added generator class Added accountmanager

---
+++ app/src/main/java/cy/agorise/crystalwallet/apigenerator/GrapheneApiGenerator.java
@@ -0,0 +1,8 @@
+package cy.agorise.crystalwallet.apigenerator;
+
+/**
+ * Created by henry on 26/9/2017.
+ */
+
+public class GrapheneApiGenerator {
+}
---
+++ app/src/main/java/cy/agorise/crystalwallet/manager/BitsharesAccountManager.java
@@ -0,0 +1,24 @@
+package cy.agorise.crystalwallet.manager;
+
+import cy.agorise.crystalwallet.models.CryptoNetAccount;
+
+/**
+ * Created by henry on 26/9/2017.
+ */
+
+public class BitsharesAccountManager implements CryptoAccountManager {
+    @Override
+    public CryptoNetAccount createAccountFromSeed(CryptoNetAccount account) {
+        return null;
+    }
+
+    @Override
+    public CryptoNetAccount importAccountFromSeed(CryptoNetAccount account) {
+        return null;
+    }
+
+    @Override
+    public void loadAccountFromDB(CryptoNetAccount account) {
+
+    }
+}
