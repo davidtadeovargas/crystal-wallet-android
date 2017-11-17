a10017edb02686e15fe83ec0f97eb659a4624030
sjaramillo10 <severiano.jaramillo@gmail.com>
2017-11-16 22:56:43 -0600

    Added custom toolbar and tab labels to board activity

--- app/src/main/java/cy/agorise/crystalwallet/activities/BoardActivity.java
+++ app/src/main/java/cy/agorise/crystalwallet/activities/BoardActivity.java
@@ -3,7 +3,6 @@ package cy.agorise.crystalwallet.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -12,7 +11,7 @@ import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+
 import android.widget.ImageButton;
 
 import butterknife.BindView;
