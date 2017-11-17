83dd0fbb3da3e476a3f1c1d8e0ff36bb6def2492
sjaramillo10 <severiano.jaramillo@gmail.com>
2017-11-16 23:43:10 -0600

    Added bottom buttons to balance, transactions and contacts

--- app/src/main/java/cy/agorise/crystalwallet/activities/BoardActivity.java
+++ app/src/main/java/cy/agorise/crystalwallet/activities/BoardActivity.java
@@ -11,6 +11,7 @@ import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.ImageButton;
 
 import butterknife.BindView;
