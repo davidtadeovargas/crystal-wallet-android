5ee178245415989026a35ab357413258407392e1
Severiano Jaramillo <severiano.jaramillo@gmail.com>
2017-12-22 21:30:20 -0600

    Created accounts fragment in animation

--- app/src/main/java/cy/agorise/crystalwallet/fragments/AccountsFragment.java
+++ app/src/main/java/cy/agorise/crystalwallet/fragments/AccountsFragment.java
@@ -42,16 +42,6 @@ public class AccountsFragment extends DialogFragment {
         super.onCreate(savedInstanceState);
     }
 
-    /*@Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_accounts, container, false);
-        ButterKnife.bind(this, view);
-
-        return view;
-    }*/
-
     @NonNull
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
