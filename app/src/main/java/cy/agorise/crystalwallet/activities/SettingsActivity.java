b524462c5e4f77b8897c76b6c33e9f125ac9528a
Severiano Jaramillo <severiano.jaramillo@gmail.com>
2017-12-28 23:07:53 -0600

    Added SettingsActivity toolbar and created GeneralSettingsFragment

--- app/src/main/java/cy/agorise/crystalwallet/activities/SettingsActivity.java
+++ app/src/main/java/cy/agorise/crystalwallet/activities/SettingsActivity.java
@@ -2,8 +2,6 @@ package cy.agorise.crystalwallet.activities;
 
 import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.os.PersistableBundle;
-import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -13,17 +11,24 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.widget.ImageView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import cy.agorise.crystalwallet.R;
 import cy.agorise.crystalwallet.fragments.BalanceFragment;
+import cy.agorise.crystalwallet.fragments.GeneralSettingsFragment;
 
 /**
- * Created by seven on 12/28/17. 
+ * Created by seven on 12/28/17.
+ *
  */
 
 public class SettingsActivity extends AppCompatActivity{
+
+    @BindView(R.id.ivGoBack)
+    public ImageView ivGoBack;
 
     @BindView(R.id.pager)
     public ViewPager mPager;
@@ -74,20 +79,19 @@ public class SettingsActivity extends AppCompatActivity{
     }
 
     private class SettingsPagerAdapter extends FragmentStatePagerAdapter {
-        public SettingsPagerAdapter(FragmentManager fm) {
+        SettingsPagerAdapter(FragmentManager fm) {
             super(fm);
         }
 
         @Override
         public Fragment getItem(int position) {
             switch (position){
-                /*case 0:
-                    return null;
-                case 1:
+                case 0:
+                    return new GeneralSettingsFragment();
+                /*case 1:
                     return null;
                 case 2:
                     return null;*/
-                case 0:
                 case 1:
                 case 2:
                 case 3:
@@ -102,5 +106,10 @@ public class SettingsActivity extends AppCompatActivity{
         public int getCount() {
             return 4;
         }
+    }
+
+    @OnClick(R.id.ivGoBack)
+    public void goBack(){
+        onBackPressed();
     }
 }
