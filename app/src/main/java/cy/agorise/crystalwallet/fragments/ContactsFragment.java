b3501a3a897c9266304414c2a767fd1d6ee530bf
Severiano Jaramillo <severiano.jaramillo@gmail.com>
2018-07-11 13:23:54 -0500

    Remove contacts list bouncy efect for now

--- app/src/main/java/cy/agorise/crystalwallet/fragments/ContactsFragment.java
+++ app/src/main/java/cy/agorise/crystalwallet/fragments/ContactsFragment.java
@@ -1,18 +1,15 @@
 package cy.agorise.crystalwallet.fragments;
 
-import android.app.Activity;
 import android.arch.lifecycle.LiveData;
 import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
 import android.arch.paging.PagedList;
-import android.graphics.Point;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,7 +18,6 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import cy.agorise.crystalwallet.R;
 import cy.agorise.crystalwallet.models.Contact;
-import cy.agorise.crystalwallet.util.BounceTouchListener;
 import cy.agorise.crystalwallet.viewmodels.ContactListViewModel;
 import cy.agorise.crystalwallet.views.ContactListAdapter;
 
@@ -31,10 +27,6 @@ public class ContactsFragment extends Fragment {
     RecyclerView rvContacts;
 
     ContactListAdapter adapter;
-
-    // Fields used to achieve bounce effect while over-scrolling the contacts list
-    private BounceTouchListener bounceTouchListener;
-    float pivotY1, pivotY2;
 
     public ContactsFragment() {
         // Required empty public constructor
@@ -64,8 +56,6 @@ public class ContactsFragment extends Fragment {
         adapter = new ContactListAdapter();
         rvContacts.setAdapter(adapter);
 
-        configureListBounceEffect();
-
         // Gets contacts LiveData instance from ContactsViewModel
         ContactListViewModel contactListViewModel =
                 ViewModelProviders.of(this).get(ContactListViewModel.class);
@@ -79,46 +69,5 @@ public class ContactsFragment extends Fragment {
         });
 
         return view;
-    }
-
-    private void configureListBounceEffect() {
-        rvContacts.setPivotX(getScreenWidth(getActivity()) * 0.5f);
-
-        pivotY1 = 0;
-        pivotY2 = (getScreenHeight(getActivity())) * .5f;
-
-        bounceTouchListener = BounceTouchListener.create(rvContacts, new BounceTouchListener.OnTranslateListener() {
-            @Override
-            public void onTranslate(float translation) {
-                if(translation > 0) {
-                    bounceTouchListener.setMaxAbsTranslation(-99);
-                    rvContacts.setPivotY(pivotY1);
-                    float scale = ((2 * translation) / rvContacts.getMeasuredHeight()) + 1;
-                    rvContacts.setScaleY((float) Math.pow(scale, .6f));
-                } else {
-                    bounceTouchListener.setMaxAbsTranslation((int) (pivotY2 * .33f));
-                    rvContacts.setPivotY(pivotY2);
-                    float scale = ((2 * translation) / rvContacts.getMeasuredHeight()) + 1;
-                    rvContacts.setScaleY((float) Math.pow(scale, .5f));
-                }
-            }
-        });
-
-        // Sets custom touch listener to handle bounce/stretch effect
-        rvContacts.setOnTouchListener(bounceTouchListener);
-    }
-
-    public static int getScreenWidth(Activity activity) {
-        Display display = activity.getWindowManager().getDefaultDisplay();
-        Point size = new Point();
-        display.getSize(size);
-        return size.x;
-    }
-
-    public static int getScreenHeight(Activity activity) {
-        Display display = activity.getWindowManager().getDefaultDisplay();
-        Point size = new Point();
-        display.getSize(size);
-        return size.y;
     }
 }
