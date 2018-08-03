f40d996282a2e3fb346b2f9eef10e0b2bbf8db71
hvarona <henry.varona@gmail.com>
2018-08-02 23:02:23 -0400

    Implementation of model for alt-coin (not graphene cryptos)
    Insight api base implementation

---
+++ app/src/main/java/cy/agorise/crystalwallet/apigenerator/insightapi/models/ScriptPubKey.java
@@ -0,0 +1,24 @@
+package cy.agorise.crystalwallet.apigenerator.insightapi.models;
+
+/**
+ * The transasction Script public keym is used to validate
+ */
+
+public class ScriptPubKey {
+    /**
+     * The code to validate in hex
+     */
+    public String hex;
+    /**
+     * the code to validate this transaction
+     */
+    public String asm;
+    /**
+     * the acoin address involved
+     */
+    public String[] addresses;
+    /**
+     * The type of the hash
+     */
+    public String type;
+}
